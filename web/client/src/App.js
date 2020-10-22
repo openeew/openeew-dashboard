92b9b54b84a2ae268824fcbb07e183044fd8ea04
James <james.kiesel@gmail.com>
2020-10-22 19:59:12 +1300

    Add router for additional routes
    Signed-off-by: James <james.kiesel@gmail.com>

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.scss';
-import Dashboard from './content/Dashboard';
-import Login from './content/Login';
+import Router from './Router';
 import AppContext, { useAppContext } from './context/app';
 
 function App() {
   return <AppContext.Provider value={useAppContext()}>
-    <Dashboard />
-    <Login />
+    <Router />
   </AppContext.Provider>
 }
 
---
+++ web/client/src/content/AccountSettings/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AccountSettings() {
+  return (
+    <div>WARK</div>
+  );
+}
+
+export default AccountSettings;
