200e8f267e6a0801ec07e75f197ba5a694019bfa
JohnyTheCarrot <tuurmartens4@gmail.com>
2021-01-21 11:14:01 +0100

    swap HashRouter out for BrowserRouter
    Signed-off-by: JohnyTheCarrot <tuurmartens4@gmail.com>

--- web/client/src/Router.js
+++ web/client/src/Router.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import { HashRouter, Switch, Route } from 'react-router-dom'
+import {Switch, Route, BrowserRouter} from 'react-router-dom'
 import Dashboard from './content/Dashboard'
 import Login from './content/Login'
 
 const Router = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <div className="app-content">
         <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/" component={Dashboard} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/events" component={Dashboard} />
+          <Route exact path="/access" component={Dashboard} />
+          <Route exact path="/settings" component={Dashboard} />
         </Switch>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
