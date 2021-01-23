a6fddb88a731922c48370e01a1864acaa7652541
Ryan Kelley <rdkelley@users.noreply.github.com>
2021-01-22 17:26:30 -0800

    Add email verification route to API
    - Add IBM CLoud IAM client to auth requests to management API
    - Add packages email-validator, node-fetch, qs
    
    Signed-off-by: Ryan Kelley <rdkelley@users.noreply.github.com>

--- web/api/rest/routes.js
+++ web/api/rest/routes.js
@@ -1,4 +1,18 @@
-const passportClient = require('../utils/passport.js');
+const fetch = require('node-fetch');
+const validator = require('email-validator');
+
+const passportService = require('../utils/passport.js');
+const IBMCloudIAM = require('../utils/IBMCloudIam');
+
+let TENET_ID;
+let services = null;
+
+if (process.env.VCAP_APPLICATION) {
+  services = JSON.parse(process.env.VCAP_SERVICES);
+  TENET_ID = services.AppID[0].credentials.tenantId;
+} else {
+  TENET_ID = require('../vcap-local.json').services.AppID.credentials.tenantId;
+}
 
 const isAuth = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -9,8 +23,73 @@ const isAuth = (req, res, next) => {
 };
 
 module.exports = (server) => {
+  /**
+   * POST /verification
+   * Calls AppID management API and verifies if email belongs to existing user
+   * @param {email} The user's email
+   * @returns {Object} User information and new account link (if new user)
+   */
+  server.express.post('/api/verification', async (req, res) => {
+    const { email } = req.body;
+    let token;
+
+    if (!email || !validator.validate(email)) {
+      return res.status(422).send('Email is missing or invalid');
+    }
+
+    try {
+      token = await IBMCloudIAM.getAccessToken();
+    } catch (e) {
+      return res
+        .status(503)
+        .send('Error authenticating API. This is an error with the server.');
+    }
+
+    const formattedEmail = email.replace('@', '%40');
+
+    const response = await fetch(
+      `https://us-south.appid.cloud.ibm.com/management/v4/${TENET_ID}/cloud_directory/Users?query=${formattedEmail}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    if (!response.ok) {
+      console.error(response);
+      return res
+        .status(503)
+        .send('There was an error verifying the email (Dependency failed)');
+    }
+
+    const json = await response.json();
+
+    if (json.totalResults === 0) {
+      return res.json({
+        verified: false,
+        firstName: '',
+        lastName: '',
+      });
+    }
+
+    return res.json({
+      verified: true,
+      firstName: json.Resources[0].name.givenName,
+      lastName: json.Resources[0].name.familyName,
+    });
+  });
+
+  /**
+   * POST /login
+   * Logins in a user, creates express session
+   * @param {string} email
+   * @param {string} password
+   * @returns {Object} User information
+   */
   server.express.post('/api/login', (req, res, next) => {
-    passportClient.authenticate((err, user, info) => {
+    passportService.authenticate((err, user, info) => {
       if (err || info) {
         return res.status(info.statusCode).json({
           errorCode: info.code,
