fa9ec2fa73e304cac58e9db2feeabf9e0380834e
Ryan Kelley <rdkelley@users.noreply.github.com>
2021-03-26 11:28:43 -0700

    feat: Add timeago formatting to utils.js
    Signed-off-by: Ryan Kelley <rdkelley@users.noreply.github.com>

--- web/client/src/utils.js
+++ web/client/src/utils.js
@@ -1,3 +1,10 @@
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+
+TimeAgo.addDefaultLocale(en)
+
+export const timeAgo = new TimeAgo('en-US')
+
 export const formatDate = (date) => {
   const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date)
   const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(date)
